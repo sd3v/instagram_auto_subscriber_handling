b477b2a4311d5168c7b10b357e5cda5cce987d5e
Sd3v <sokkadev@gmx.de>
2019-10-16 20:56:56 +0200

    TODOs

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import selenium
+import requests
+import bs4 as BeautifulSoup
+
+
+
+
+def login(u,p):
+    #TODO Login 
+
+
+
+def main():
+    #TODO Main Function
+
+
+def navigate():
+    #Navigate to Notification Site
+
+
+def refresh(url):
+    #Refresh Insta Notif Site every n minutes
+
+
+#TODO Error Handling
+   # --> Network Errors
+   # --> Login Fails
+   # --> TimeOuts etc..
