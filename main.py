efea03f0125b0ae89a0b28f255378a62f46e610b
SDev <56226730+sd3v@users.noreply.github.com>
2020-04-27 10:10:47 +0200

    No need for Bs4

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
-import bs4 as BeautifulSoup
 import time
 from getpass import getpass
 import datetime
